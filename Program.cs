ccc819b19ad0fc6fb2b357d0ac56dbe67475312a
Toporik7 <149801581+Toporik7@users.noreply.github.com>
2023-12-25 14:26:12 +0300

    Задача 3

--- Program.cs
+++ Program.cs
@@ -1,21 +1,63 @@
-﻿var array = new int[4, 4]
-            { { 1, 2, 3, 4 },
-              { 0, 0, 0, 0 },
-              { 0, 0, 0, 0 },
-              { 5, 6, 7, 8 }
-            };
-            for(int i = 0; i < array.GetLength(1); i++)
-            {
-                var tmp = array[3, i];
-                array[3, i] = array[0, i];
-                array[0, i] = tmp;
-            }
-            for(int i = 0; i < array.GetLength(0); i++)
-            {
-                for(int j = 0; j < array.GetLength(1); j++)
-                {
-                    Console.Write(array[i,j] + " ");
-                }
-                Console.WriteLine();
-            }
-            Console.ReadKey(true);
+﻿Console.WriteLine("Введите размер массива m x n и диапазон случайных значений:");
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+int range = InputNumbers("Введите диапазон: от 1 до ");
+
+int[,] array = new int[m, n];
+CreateArray(array);
+WriteArray(array);
+
+int minSumLine = 0;
+int sumLine = SumLineElements(array, 0);
+for (int i = 1; i < array.GetLength(0); i++)
+{
+  int tempSumLine = SumLineElements(array, i);
+  if (sumLine > tempSumLine)
+  {
+    sumLine = tempSumLine;
+    minSumLine = i;
+  }
+}
+
+Console.WriteLine($"\n{minSumLine+1} - строкa с наименьшей суммой ({sumLine}) элементов ");
+
+
+int SumLineElements(int[,] array, int i)
+{
+  int sumLine = array[i,0];
+  for (int j = 1; j < array.GetLength(1); j++)
+  {
+    sumLine += array[i,j];
+  }
+  return sumLine;
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(range);
+    }
+  }
+}
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
